298b39a65a5aea16fa9e901ab74388dc8b214411
tayfundagci <tayfundagci3517@gmail.com>
2021-10-09 01:39:41 +0300

    add templete literals js

---
+++ JavaScript/js/06-templete-literals.js
@@ -0,0 +1,24 @@
+let username = "tayfun"
+const DOMAIN = "kodluyoruz.org"
+
+let email = username + "@" + DOMAIN
+ 
+/* 
+console.log("Merhaba", username, "sitemize hoşgeldin", "mail adresin: ", email)
+*/
+
+
+
+let info = `
+Merhaba ${username} sitemize hoşgeldin.. 
+mail adresin: ${email}
+
+mail adresinin uzunluğu: ${email.length}
+Borcunuz: ${(2 + 3) * 10} TL
+günün saat bilgisi : ${new Date().getHours()}
+
+kısa isminiz: ${username[0]}.
+`
+
+console.log(info)
+
