edc9daf852a9b7c78ad988a5ab1df7cb0f09b2d9
tayfundagci <tayfundagci3517@gmail.com>
2021-10-09 13:48:24 +0300

    add dom

---
+++ JavaScript/js/08-document-object-model.js
@@ -0,0 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model
+// https://www.w3schools.com/js/js_htmldom_css.asp
+
+console.log(document.URL)
+console.log(document.location)
+console.log(document.location.hostname)
+console.log(document.location.pathname)
+
+console.log(document.head)
+console.log(document.body)
+
+document.body.style.backgroundColor = "aqua"
