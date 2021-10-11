b81672dd388f77e9d149d187c30ab2075462367c
tayfundagci <tayfundagci3517@gmail.com>
2021-10-11 21:41:19 +0300

    change 14-kosullar JS

--- JavaScript/js/14-kosullar.js
+++ JavaScript/js/14-kosullar.js
@@ -1,4 +1,13 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
 
+let username = prompt("Kullanıcı Adınızı Giriniz:")
+// eger kullanici bilgisi varsa ekrana ismini yazdir
+// eger (username.length > 0) {console.log(username)} degilse {console.log("bilgi yok")}
+// if (username.length > 0) {console.log(username)} else {console.log("bilgi yok")}
 
-// kullanici bilgisi varsa ekrana ismini yazdir
+if (username) { // if kısmı her zaman true ise çalışır
+    console.log(`Kullanıcı Bilginiz : ${username}`)
+}
+else {
+    console.log("Bilgi Yok")
+}
