914bf153f13c58d225f7afff537f3de1b9a75988
tayfundagci <tayfundagci3517@gmail.com>
2021-10-12 18:21:46 +0300

    add fat arrow JS

---
+++ JavaScript/js/20-arrow-function.js
@@ -0,0 +1,34 @@
+function hello(firstName) {
+    console.log(`Merhaba ${firstName}`)
+}
+
+hello("JavaScript")
+
+
+
+const helloFuncV1 = (firstName) => 
+{ 
+    console.log(`Merhaba ${firstName}`)
+}
+helloFuncV1("helloFuncV1")
+
+
+
+const helloFuncV2 = firstName => console.log(`Merhaba ${firstName}`)
+helloFuncV2("helloFuncV2")
+// bir parametre ve bir dönüş işlemi var ise, parantez ile süslü paranteze gerek yok
+
+
+const helloFuncV3 = (firstName, lastName) => 
+console.log(`Merhaba ${firstName} ${lastName}`)
+helloFuncV3("helloFuncV3", "Last Name Info")
+
+
+
+const helloFuncV4 = (firstName, lastName) => 
+{
+    let info = `Merhaba ${firstName} ${lastName}`
+    console.log(info)
+    return info
+}
+helloFuncV4("helloFuncV4", "Other Info")
