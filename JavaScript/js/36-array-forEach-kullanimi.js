6f50ef2d63ffaf4fe675b7ccaeba21b107f00a6c
tayfundagci <tayfundagci3517@gmail.com>
2021-10-15 12:59:04 +0300

    add foreach JS

---
+++ JavaScript/js/36-array-forEach-kullanimi.js
@@ -0,0 +1,27 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+// https://www.w3schools.com/jsref/jsref_foreach.asp
+
+const PRODUCTS = ["Laptop", "Phone", "Speaker", "Desktop PC", "Server", "Mouse", "Keyboard"]
+
+// PRODUCTS.forEach((product, index, array) => array[index] = product + " 111" )
+PRODUCTS.forEach((product, index, array) => array[index] = `${product.toUpperCase()}` ) // touppercase
+
+console.log(PRODUCTS)
+
+/*
+const userListDOM = document.querySelector('#userList')
+
+for (index = 0; index < users.length; index++) {
+    const liDOM = document.createElement('li')
+    liDOM.innerHTML = users[index]
+    userListDOM.appendChild(liDOM)
+}
+*/
+
+const userListDOM = document.querySelector('#userList')
+
+PRODUCTS.forEach( item => {
+    const liDOM = document.createElement('li')
+    liDOM.innerHTML = item
+    userListDOM.append(liDOM)
+})
