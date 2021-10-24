e25c4574314bd07141d2113917a5a0d1b59f540a
tayfundagci <tayfundagci3517@gmail.com>
2021-10-24 14:03:47 +0300

    change coklukosulyapisi

--- JavaScript/js/15-coklukosulyapisi.js
+++ JavaScript/js/15-coklukosulyapisi.js
@@ -1,42 +1,44 @@
-let username = prompt("Kullanıcı Adınız:")
-let age = prompt("Yaşınız:")
-let info = document.querySelector("#info")
+// let username = prompt("Kullanıcı Adınız:")
+// let age = prompt("Yaşınız:")
+// let info = document.querySelector("#info")
 
-if(username && age >= 18)
-{
-    info.innerHTML = "ehliyet alabilirsiniz."
-}
-else if(!username)
-{
-    info.innerHTML = "Kullanıcı Adınız Yok"
-}
-else if( !(age >= 18) )
-{
-    info.innerHTML = "Yaş Bilginiz Yok veya 18 Yaşından Küçüksünüz"
-}
+// if(username && age >= 18)
+// {
+//     info.innerHTML = "ehliyet alabilirsiniz."
+// }
+// else if(!username)
+// {
+//     info.innerHTML = "Kullanıcı Adınız Yok"
+// }
+// else if( !(age >= 18) )
+// {
+//     info.innerHTML = "Yaş Bilginiz Yok veya 18 Yaşından Küçüksünüz"
+// }
 
 
 // --------------------------------------------------------
 
-// let hava = "Karlı";
-// switch(hava) {
-//   case "Yagmurlu":
-//       console.log("Semsiyeni yanina almayi unutma");
-//       break;
-//   case "Gunesli":
-//       console.log("Hafif giyin");
-//   case "Bulutlu":
-//       console.log("Disari cik");
-//       break;
-//   case "Karlı":
-//       console.log("Kalin giyin");
-//       break;
-//   case "Firtinali":
-//       console.log("Bir süre disari cikma");
-//       break;
-//   default:
-//       console.log("Bilinmeyen hava durumu:" , hava);
-// }
+let havadurumu = prompt("Hava Durumunu Giriniz")
+let hava = document.querySelector("#info")
+switch(havadurumu) {
+  case "Yagmurlu":
+      info.innerHTML = "Semsiyeni yanina almayi unutma"
+      break;
+  case "Gunesli":
+      info.innerHTML = "Hafif giyin"
+      break;
+  case "Bulutlu":
+      info.innerHTML = "Disari cik"
+      break;
+  case "Karlı":
+      info.innerHTML = "Kalin giyin"
+      break;
+  case "Firtinali":
+      info.innerHTML = "Bir süre disari cikma"
+      break;
+  default:
+      info.innerHTML = `Bilinmeyen Hava Durumu: ${havadurumu}`
+}
 
 // ----------------- HESAP MAKİNESİ -----------------------
 
