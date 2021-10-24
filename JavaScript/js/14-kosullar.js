6564cb0df2e3fcd1370091aa20ae5dd90ce63423
tayfundagci <tayfundagci3517@gmail.com>
2021-10-24 14:03:35 +0300

    change ifelse

--- JavaScript/js/14-kosullar.js
+++ JavaScript/js/14-kosullar.js
@@ -7,40 +7,42 @@
 // eger (username.length > 0) {console.log(username)} degilse {console.log("bilgi yok")}
 // if (username.length > 0) {console.log(username)} else {console.log("bilgi yok")}
 
-let username = prompt("Kullanıcı Adınızı Giriniz:")
 
-if (username) { // if kısmı her zaman true ise çalışır
-    console.log(`Kullanıcı Bilginiz : ${username}`)
-}
-else {
-    console.log("Bilgi Yok")
-}
+
+// let username = prompt("Kullanıcı Adınızı Giriniz:")
+
+// if (username) { // if kısmı her zaman true ise çalışır
+//     console.log(`Kullanıcı Bilginiz : ${username}`)
+// }
+// else {
+//     console.log("Bilgi Yok")
+// }
 
 
 
 
 // ------------------ geçti kaldı --------------------
 
-// var ogrVizeNot = 68; // Bu kod satırını değiştiriniz. 
-// var ogrFinalNot = 80; // Bu kod satırını değiştiriniz. 
+var ogrVizeNot = 35; // Bu kod satırını değiştiriniz. 
+var ogrFinalNot = 20; // Bu kod satırını değiştiriniz. 
 
-// var ogrOrtalama = (ogrVizeNot * 0.3) + (ogrFinalNot * 0.7);
+var ogrOrtalama = (ogrVizeNot * 0.3) + (ogrFinalNot * 0.7);
 
-// if(ogrOrtalama > 0 && ogrOrtalama < 30) {
-//   console.log("Not ortalamanız: " +ogrOrtalama + " KALDINIZ(FF).");
-// }
+if(ogrOrtalama > 0 && ogrOrtalama < 30) {
+  console.log("Not ortalamanız: " +ogrOrtalama + " KALDINIZ(FF).");
+}
 
-// else if(ogrOrtalama > 31 && ogrOrtalama < 49) {
-//   console.log("Not ortalamnız: " +ogrOrtalama + " DC - KOŞULLU ");
-// }
+else if(ogrOrtalama > 31 && ogrOrtalama < 49) {
+  console.log("Not ortalamnız: " +ogrOrtalama + " DC - KOŞULLU ");
+}
 
-// else if(ogrOrtalama > 50 && ogrOrtalama < 84) {
-//   console.log("Not ortalamnız: " +ogrOrtalama + " CC - GEÇTİNİZ ");
-// }
+else if(ogrOrtalama > 50 && ogrOrtalama < 84) {
+  console.log("Not ortalamnız: " +ogrOrtalama + " CC - GEÇTİNİZ ");
+}
 
-// else if(ogrOrtalama > 85 && ogrOrtalama <= 100) {
-//   console.log("Not ortalamnız: " +ogrOrtalama + " AA - GEÇTİNİZ ");
-// }
+else if(ogrOrtalama > 85 && ogrOrtalama <= 100) {
+  console.log("Not ortalamnız: " +ogrOrtalama + " AA - GEÇTİNİZ ");
+}
 
 
 
